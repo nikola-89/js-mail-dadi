371255a7baf242df1b37e6397f49fa9f87b61e97
Nicola <nicola.ba1989@gmail.com>
2020-01-07 14:38:07 +0100

    update dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,11 +3,11 @@
 var numUser0 = parseInt(prompt('USER_0 = Inserisci un numero da 1 a 6:'));
 var numUser1 = parseInt(prompt('USER_1 = Inserisci un numero da 1 a 6:'));
 
-if(isNaN(numUser0) || isNaN(numUser1)) {
+if(isNaN(numUser0) && isNaN(numUser1)) {
     alert('Non hai inserito un numero.');
 }
 else {
-    if(numUser0 && numUser1 <= 6 ||	numUser0 && numUser1 !=	0) {
+    if(numUser0 <= 6 && numUser1 <= 6 && numUser0 != 0 && numUser1 != 0) {
         if(numUser0 > numUser1) {
             alert('USER_0 Vince.');
         }
