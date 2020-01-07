e5de28ef29c2d0f2aecd9a4cdec1f9e89d8545d8
Nicola <nicola.ba1989@gmail.com>
2020-01-07 14:22:59 +0100

    update dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,17 +3,22 @@
 var numUser0 = parseInt(prompt('USER_0 = Inserisci un numero da 1 a 6:'));
 var numUser1 = parseInt(prompt('USER_1 = Inserisci un numero da 1 a 6:'));
 
-if(numUser0 && numUser1 <= 6 ||	numUser0 && numUser1 !=	0) {
-    if(numUser0 > numUser1) {
-        alert('USER_0 Vince.');
-    }
-    else if(numUser0 == numUser1) {
-        alert('Pari.');
-    }
-    else {
-        alert('USER_1 Vince.');
-    }
+if(isNaN(numUser0) || isNaN(numUser1)) {
+    alert('Non hai inserito un numero.');
 }
 else {
-    alert('Numero non consentito.');
+    if(numUser0 && numUser1 <= 6 ||	numUser0 && numUser1 !=	0) {
+        if(numUser0 > numUser1) {
+            alert('USER_0 Vince.');
+        }
+        else if(numUser0 == numUser1) {
+            alert('Pari.');
+        }
+        else {
+            alert('USER_1 Vince.');
+        }
+    }
+    else {
+        alert('Numero non consentito.');
+    }
 }
